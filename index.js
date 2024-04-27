214d0bd9e14fafbe1264d5bb00e55eef582481c6
gfeelixsantos <felix.devx@gmail.com>
2024-04-26 21:52:43 -0300

    update config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-var porta = process.env.PORT_APP || 3001
+var porta = process.env.PORT || 3001
  
 app.get('/', (req, res) => {
     res.send('ok')
