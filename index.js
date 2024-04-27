37acad17aeff15a2d9144274330befdedaf77aae
gfeelixsantos <felix.devx@gmail.com>
2024-04-26 23:39:19 -0300

    update index.js

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     
-    console.log('REQUISIÇÃO BODY', req.body);
-    console.log('REQUISIÇÃO DATA', req.data);
-    console.log('REQUISIÇÃO FILES', req.files);
-    console.log('REQUISIÇÃO FILE', req.file);
-    console.log('REQUISIÇÃO EVENT', req.event);
+    console.log('REQUISIÇÃO', req);
 })
 
 
