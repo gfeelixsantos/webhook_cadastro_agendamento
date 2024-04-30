ecf6f740077edb516b24d5ea0284139cd4f3ad2e
gfeelixsantos <felix.devx@gmail.com>
2024-04-30 10:01:36 -0300

    update files

--- soc/aso/riscos.js
+++ soc/aso/riscos.js
@@ -2,7 +2,7 @@ async function getRisks(agendamento) {
 
     try {
         const url = `https://ws1.soc.com.br/WebSoc/exportadados?parametro={"empresa":'16459',"codigo":"193602","chave":"8355c87bb9157db187cb","tipoSaida":"json","empresaTrabalho":"${agendamento.codEmpresa}","funcionario":"${agendamento.codFuncionario}"}`;
-        console.log(url);
+        
         const response = await fetch(url);
         const responseBuff = await response.arrayBuffer();
         const riscos = new TextDecoder('iso-8859-1').decode(responseBuff);
--- soc/exame/examRequestXml.js
+++ soc/exame/examRequestXml.js
@@ -92,11 +92,7 @@ function setProviderCode(exame, agendamento) {
     // Laboratorio: Antares após 12hrs senão Cedill
     if (exame['CODIGOEXAME'] == '2' || exame['CODIGOEXAME'] == 'XX' || exame['CODIGOEXAME'] == '28.15.001-5' || exame['CODIGOEXAME'] == '28.15.003-1' || exame['CODIGOEXAME'] == '28.15.004-0' || exame['CODIGOEXAME'] == '28.15.005-8' || exame['CODIGOEXAME'] == '28.15.006-6' || exame['CODIGOEXAME'] == '5555' || exame['CODIGOEXAME'] == '28010175' || exame['CODIGOEXAME'] == '28.15.009-0' || exame['CODIGOEXAME'] == '28.15.012-0' || exame['CODIGOEXAME'] == '1332' || exame['CODIGOEXAME'] == '1222' || exame['CODIGOEXAME'] == '00000' || exame['CODIGOEXAME'] == '28010507' || exame['CODIGOEXAME'] == '28.01.054-0' || exame['CODIGOEXAME'] == '02' || exame['CODIGOEXAME'] == '28100239' || exame['CODIGOEXAME'] == '28.15.030-9' || exame['CODIGOEXAME'] == '28.15.014-7' || exame['CODIGOEXAME'] == '22333' || exame['CODIGOEXAME'] == '002000' || exame['CODIGOEXAME'] == '28.01.095-7' || exame['CODIGOEXAME'] == '28.01.097-3' || exame['CODIGOEXAME'] == '13b' || exame['CODIGOEXAME'] == '2336' || exame['CODIGOEXAME'] == '28040350' || exame['CODIGOEXAME'] == '28011023' || exame['CODIGOEXAME'] == '28.04.048-1' || exame['CODIGOEXAME'] == '28040562' || exame['CODIGOEXAME'] == '28060105' || exame['CODIGOEXAME'] == '28060113' || exame['CODIGOEXAME'] == '28060067' || exame['CODIGOEXAME'] == '-' || exame['CODIGOEXAME'] == '144' || exame['CODIGOEXAME'] == '1123' || exame['CODIGOEXAME'] == '00022' || exame['CODIGOEXAME'] == '1125' || exame['CODIGOEXAME'] == '200' || exame['CODIGOEXAME'] == '28.15.018-0' || exame['CODIGOEXAME'] == '1' || exame['CODIGOEXAME'] == '54778844' || exame['CODIGOEXAME'] == '28030141' || exame['CODIGOEXAME'] == '0101' || exame['CODIGOEXAME'] == '28.13.036-7' || exame['CODIGOEXAME'] == '47788855' || exame['CODIGOEXAME'] == '28061004' || exame['CODIGOEXAME'] == '09022023' || exame['CODIGOEXAME'] == '02020' || exame['CODIGOEXAME'] == '1 b' || exame['CODIGOEXAME'] == '28.01.136-8' || exame['CODIGOEXAME'] == '28.01.137-6' || exame['CODIGOEXAME'] == '28011392' || exame['CODIGOEXAME'] == '28.01.141-4')
     {
-        let horaAgendamento = agendamento.horario.split(':')
-        horaAgendamento = Number(horaAgendamento[0])
-
-        const prestadorLaboratorio = horaAgendamento > 12 ? 61 : 32
-        return prestadorLaboratorio
+        return 32
     }
 
     else return 33
