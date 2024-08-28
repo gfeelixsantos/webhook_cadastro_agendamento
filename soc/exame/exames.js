9fa207ca137dd7e9aba7af705cb292d05a1dc425
gfeelixsantos <felix.devx@gmail.com>
2024-08-28 09:24:26 -0300

    adjust url getEmployeeExams

--- soc/exame/exames.js
+++ soc/exame/exames.js
@@ -3,8 +3,7 @@ async function getEmployeeExams(agendamento) {
     const anoAtual = new Date().getFullYear()
 
     try {
-        const url = `https://ws1.soc.com.br/WebSoc/exportadados?parametro=
-        {"empresa":"16459","codigo":"193598","chave":"c9184b11650a305f8a6e","tipoSaida":"json","empresaTrabalho":"${agendamento.empresa.codEmpresa}","funcionarios":"${agendamento.funcionario.codFuncionario}","periodo":"12/${anoAtual}","exame":"","convocarClinico":"0","nuncaRealizados":"","periodicosNuncaRealizados":"","selecao":"1","examesPendentes":"","convocaPendentesPCMSO":""}`;
+        const url = `https://ws1.soc.com.br/WebSoc/exportadados?parametro={"empresa":"16459","codigo":"193598","chave":"c9184b11650a305f8a6e","tipoSaida":"json","empresaTrabalho":"${agendamento.empresa.codEmpresa}","funcionarios":"${agendamento.funcionario.codFuncionario}","periodo":"12/${anoAtual}","exame":"","convocarClinico":"0","nuncaRealizados":"","periodicosNuncaRealizados":"","selecao":"1","examesPendentes":"","convocaPendentesPCMSO":""}`;
         
         const response = await fetch(url);
         const responseBuff = await response.arrayBuffer();
