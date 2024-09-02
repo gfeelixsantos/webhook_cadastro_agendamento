87b4c1bc14412c950a3800cd7d69b9b5c41e3784
gfeelixsantos <felix.devx@gmail.com>
2024-09-02 17:53:21 -0300

    wip: include admissional process

--- new.js
+++ new.js
@@ -24,7 +24,7 @@ app.post('/', async (req, res) => {
 start()
 async function start() {
     let agendamento = await formulariosRecebidos()
-    exameAdmissional(agendamento)
+    exameAdmissional(mock)
         .then( () => console.log( '------------------------------------------------------ >> Agendamento Finalizado!'))
         .catch( e => console.log( e, 'Erro de agendamento!')) 
     
